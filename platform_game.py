3eb76f0bedac1c63868c861d742845091e9ce31a
David Arico <davidarico@me.com>
2018-03-01 14:42:11 -0800

    Add files via upload
    added the main file of the platform game

---
+++ platform_game.py
@@ -0,0 +1,25 @@
+
+# platform_game.py
+# By: {group members names}
+# Date created: March 1st, 2018
+# This program is a simple game that was made by SMC programming club members.
+# Game is meant to be a simple platform game with eniemies, platforms, lives,
+# a timer, and an end
+# Built with the pygame library.
+
+#Iimporting libraries that are needed to run the game
+import pygame
+import sys
+
+# Importanting classes from seperate files in order to be organized
+
+
+# Game needs Pyhthon 3 and higher in order to be ran without errors
+if sys.version_info[0] < 3:
+    raise "Ran with Python 2. Needed Python 3."
+
+def main():
+    pass
+
+if __name__ =="__main__":
+    main()
