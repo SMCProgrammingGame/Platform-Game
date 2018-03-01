8c6cbd1f693e39b20e826b7ad3e0d84a6d3df0a4
David Arico <davidarico@me.com>
2018-03-01 15:23:49 -0800

    Add files via upload

---
+++ lib/menu.py
@@ -0,0 +1,9 @@
+
+# menu.py
+# class that will handle the startup of the game and will feature a simple
+# interface that will have options to quit the game or to start the game
+
+class Menu:
+    
+    def __init__(self, sc):
+        self.screen = sc
